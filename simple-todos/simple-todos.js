f1c6bf8972651a55d36fdf67088a72a688c6ad50
cryptolemming <aliayoub22@gmail.com>
2016-03-14 15:53:33 -0400

    added functionality to only allow the user to update or delete

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -42,12 +42,10 @@ if (Meteor.isClient) {
   Template.task.events({
     "click .toggle-checked": function () {
       // Set the checked property to the opposite of its current value
-      Tasks.update(this._id, {
-        $set: {checked: ! this.checked}
-      });
+      Meteor.call("setChecked", this._id, ! this.checked);
     },
     "click .delete": function () {
-      Tasks.remove(this._id);
+      Meteor.call("deleteTask", this._id);
     }
   });
 
@@ -71,9 +69,16 @@ Meteor.methods({
     });
   },
   deleteTask: function (taskId) {
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
     Tasks.remove(taskId);
   },
   setChecked: function (taskId, setChecked) {
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
     Tasks.update(taskId, { $set: { checked: setChecked} });
   }
+
 });
