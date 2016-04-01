83b300b2fd6d9ad464963b2d1c0f8edfb686413b
cryptolemming <aliayoub22@gmail.com>
2016-04-01 15:46:15 -0400

    created test to delete mock task

--- react-meteor/imports/api/tasks.js
+++ react-meteor/imports/api/tasks.js
@@ -6,7 +6,7 @@ export const Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isServer) {
 	Meteor.publish('tasks', function tasksPublication() {
-		returns Tasks.find({
+		return Tasks.find({
 			$or: [
 				{ private: {$ne: true } },
 				{ owner: this.userId },
