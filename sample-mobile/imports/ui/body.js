6293f383aea962b64aa4cb67da3cdc5866bb3d51
cryptolemming <aliayoub22@gmail.com>
2016-05-01 13:51:23 -0400

    initial before starting mobile add

---
+++ sample-mobile/imports/ui/body.js
@@ -0,0 +1,28 @@
+import { Template } from 'meteor/templating';
+import { Tasks } from '../api/tasks.js';
+
+import './task.js';
+import './body.html';
+
+Template.body.helpers({
+	tasks() {
+		return Tasks.find({}, {sort: {createdAt: -1}});
+	}
+});
+
+Template.body.events({
+	'submit .new-task'(e) {
+		e.preventDefault();
+
+		const target = e.target;
+		const text = target.text.value;
+
+		Tasks.insert({
+			text,
+			createdAt: new Date(),
+		});
+
+		target.text.value = '';
+	}
+})
+
