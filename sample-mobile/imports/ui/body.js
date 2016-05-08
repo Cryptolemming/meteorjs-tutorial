55f274d60f102c707691dee603974e6451491825
cryptolemming <aliayoub22@gmail.com>
2016-05-08 08:25:32 -0400

    finished demo

--- sample-mobile/imports/ui/body.js
+++ sample-mobile/imports/ui/body.js
@@ -1,13 +1,29 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
 import { Tasks } from '../api/tasks.js';
+
 
 import './task.js';
 import './body.html';
 
+Template.body.onCreated( function bodyOnCreated() {
+	this.state = new ReactiveDict();
+	Meteor.subscribe('tasks');
+});
+
 Template.body.helpers({
 	tasks() {
-		return Tasks.find({}, {sort: {createdAt: -1}});
-	}
+		const instance = Template.instance();
+		if (instance.state.get('hideCompleted')) {
+			return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+		}
+		return Tasks.find({}, {sort: { createdAt: -1 } });
+	},
+	incompleteCount() {
+		return Tasks.find({ checked: { $ne: true } }).count();
+	},
 });
 
 Template.body.events({
@@ -17,12 +33,12 @@ Template.body.events({
 		const target = e.target;
 		const text = target.text.value;
 
-		Tasks.insert({
-			text,
-			createdAt: new Date(),
-		});
+		Meteor.call('tasks.insert', text);
 
 		target.text.value = '';
-	}
-})
+	},
+	'change .hide-completed input'(e, instance) {
+		instance.state.set('hideCompleted', event.target.checked);
+	},
+});
 
